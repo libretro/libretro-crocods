5a66ad29f45af6e19b77bd9c331fa2a59ec0d92d
RedBug <github@kyuran.be>
2017-02-28 17:54:33 +0100

    Update install_recalbox.sh

--- install_recalbox.sh
+++ install_recalbox.sh
@@ -16,12 +16,14 @@ mount -o remount, rw /
 curl https://github.com/redbug26/libretro-crocods/releases/download/version/crocods_libretro.so -o /usr/lib/libretro/crocods_libretro.so
 
 # Add .kcr file extension in emulation station
-xml ed -inplace -u "/systemList/system[name='amstradcpc']/extension" -v ".dsk .DSK .zip .ZIP .kcr .KCR" $CONFIG
+xml ed -u "/systemList/system[name='amstradcpc']/extension" -v ".dsk .DSK .zip .ZIP .kcr .KCR" $CONFIG >$CONFIG.new
+cp $CONFIG.new $CONFIG
 
 # Add crocods core in emulation station if not exist yet
 count=`xml sel -t -v "count(/systemList/system[name='amstradcpc']/emulators/emulator/cores[core='crocods'])" $CONFIG`
 if [ "$count" == "0" ]; then
-        xml ed -inplace -s "/systemList/system[name='amstradcpc']/emulators/emulator/cores" -t elem -n core -v "crocods" -i core -t attr -n class -v com.foo $CONFIG > $CONFIG.new
+        xml ed -s "/systemList/system[name='amstradcpc']/emulators/emulator/cores" -t elem -n core -v "crocods" -i core -t attr -n class -v com.foo $CONFIG > $CONFIG.new
+        cp $CONFIG.new $CONFIG
 fi
 
 
